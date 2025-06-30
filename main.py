2d1dd989bd3a416499d390c4b6c734fe25da7105
sobreiracaio <caiosobreira@gmail.com>
2025-06-30 18:08:04 -0300

    navbar

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+users_list = ["Caio", "Ze", "Jão", "Manuel"]
+
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/users')
+def users():
+    return render_template('users.html', users_list = users_list)
 
 
 if __name__ == '__main__':
