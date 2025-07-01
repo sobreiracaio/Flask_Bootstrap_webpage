d68bffbe365710a7678dd1c8ea9b959887e26a6d
sobreiracaio <caiosobreira@gmail.com>
2025-07-01 15:40:20 -0300

    form formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCreateAccount
+
 
 app = Flask(__name__)
 
@@ -20,10 +21,19 @@ def contact():
 def users():
     return render_template('users.html', users_list = users_list)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_create_acc = FormCreateAccount()
+    
+    if form_login.validate_on_submit() and 'submit_button_login' in request.form:
+        flash(f"Login successfully done on email: {form_login.email.data}!", 'alert-success')
+        return redirect(url_for('home'))
+        
+    if form_create_acc.validate_on_submit() and 'submit_button_create_acc' in request.form :
+        flash(f"Account created successfully for email: {form_create_acc.email.data}", 'alert-success')
+        return redirect(url_for('home'))
+        
     return render_template('login.html', form_login=form_login, form_create_acc=form_create_acc)
 
 
