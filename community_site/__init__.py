de9e5a0514c528604bc6e2af2951dabc379120ab
sobreiracaio <caiosobreira@gmail.com>
2025-07-02 14:08:24 -0300

    encrypting passwords

--- community_site/__init__.py
+++ community_site/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = 'a1e4c51b5a440f3fbf1f3e96126a354a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///community.db'
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from community_site import routes
--- community_site/routes.py
+++ community_site/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, request, flash
-from community_site import app
+from community_site import app, database, bcrypt
 from community_site.forms import FormLogin, FormCreateAccount
+from community_site.models import User
 
 
 
@@ -28,6 +29,10 @@ def login():
         return redirect(url_for('home'))
         
     if form_create_acc.validate_on_submit() and 'submit_button_create_acc' in request.form :
+        crypt_pass = bcrypt.generate_password_hash(form_create_acc.password.data)
+        user = User(username=form_create_acc.username.data, email=form_create_acc.email.data, password= crypt_pass) #create user
+        database.session.add(user) #add a session
+        database.session.commit() #commit session
         flash(f"Account created successfully for email: {form_create_acc.email.data}", 'alert-success')
         return redirect(url_for('home'))
         
