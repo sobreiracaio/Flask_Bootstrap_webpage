9d0219c78d1d359283b17fc55a19f071f36a9b32
sobreiracaio <caiosobreira@gmail.com>
2025-07-06 13:54:31 -0300

    init

--- community_site/__init__.py
+++ community_site/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+import os
 
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = 'a1e4c51b5a440f3fbf1f3e96126a354a'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///community.db'
+
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///community.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
