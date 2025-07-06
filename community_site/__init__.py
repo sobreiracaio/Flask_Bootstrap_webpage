120df705084cd3ec3ba84ac4a630745ad43bd042
sobreiracaio <caiosobreira@gmail.com>
2025-07-06 14:29:11 -0300

    database setup

--- community_site/__init__.py
+++ community_site/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -19,5 +20,20 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+
+from community_site import models
+
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+
+if not inspector.has_table("user"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Database created")
+else:
+    print("Database already exists")
+
 
 from community_site import routes
